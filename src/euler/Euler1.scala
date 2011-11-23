ec5e7fdb1ed72d8dce02ebde862721485a841a45
shadaj <shadaj@shadajs-mac-mini.local>
2011-11-23 14:48:21 -0800

    Fixed bug that included 1000 in the range for Euler 1

--- src/euler/Euler1.scala
+++ src/euler/Euler1.scala
@@ -3,7 +3,7 @@ package euler
 // Add all the natural numbers below one thousand that are multiples of 3 or 5.
 object Euler1 extends App {
   
-    val sum = (1 to 1000).filter(n => n % 3 == 0 || n % 5 == 0).sum
+    val sum = (1 until 1000).filter(n => n % 3 == 0 || n % 5 == 0).sum
     println(sum)
 
 }
