0d2aa6a602cebe15cdc0f908cc97866a24ca27d6
shadaj <shadaj@shadajs-mac-mini.local>
2012-02-08 19:14:25 -0800

    Use more funtional style

--- src/euler/Euler3.scala
+++ src/euler/Euler3.scala
@@ -4,40 +4,14 @@ import scala.annotation.tailrec
 // Find the largest prime factor of a composite number.
 
 object Euler3 extends App {
-  def isPrime(num: Long) = {
-    if (num == 2) {
-      true
-    }
-    var i = 2
-    var found = false
-    while (found == false && i <= Math.sqrt(num)) {
-      if (num % i == 0) {
-        found = true
-      } else {
-        i = i + 1
-      }
-    }
-    !found
-  }
-  @tailrec
-  def primeFactorization(num: Long, curList: List[Long] = List()): List[Long] = {
-    var i = 2
-    var found = false
-    while (!found && i <= num / 2) {
-      if (num % i == 0 && isPrime(i)) {
-        found = true
-      } else {
-        i = i + 1
-      }
-
-    }
-    if (found) {
-      primeFactorization(num / i, i :: curList)
-    } else {
-      num :: curList
+  def primeFactors(num: Long): List[Long] = {
+    val exists = (2L to math.sqrt(num).toLong).find(num % _ == 0)
+    exists match {
+      case Some(d) => d :: primeFactors(num/d)
+      case None => List(num)
     }
   }
 
-  println(primeFactorization(600851475143L).head)
+  println(primeFactors(600851475143L).last)
 
 }
