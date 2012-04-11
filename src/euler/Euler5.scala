11ff43db59254c6345967a95e142d0ac19af27a1
shadaj <shadaj>
2012-04-10 19:18:08 -0700

    Removed vars from Euler5

--- src/euler/Euler5.scala
+++ src/euler/Euler5.scala
@@ -3,19 +3,12 @@ package euler
 // What is the smallest number divisible by each of the numbers 1 to 20?
 
 object Euler5 extends App {
-  def divisibleBy(num: Int): Boolean = {
+  def divisibleByNums(num: Int): Boolean = {
     (20 to 2 by -1).forall(num % _ == 0)
   }
-
-  var i = 21
-  var found = false
-  while (!found) {
-    if (divisibleBy(i)) {
-      found = true
-    } else {
-      i = i + 1
-    }
-  }
-  println(i)
-
+  
+  val foundNum = Iterator.from(2).find(divisibleByNums(_))
+  
+  // if number does not exist, find will never end anyway
+  println(foundNum.get)
 }
