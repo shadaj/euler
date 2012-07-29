b3fa1d68fd924eabbdf1a574d146418da1aba576
shadaj <shadaj>
2012-07-29 13:48:17 -0700

    Better (and faster using view!) implementation of Euler9

--- src/euler/Euler9.scala
+++ src/euler/Euler9.scala
@@ -2,19 +2,27 @@ package euler
 
 //A Pythagorean triplet is a set of three natural numbers, a  b  c, for which,
 //
-//a2 + b2 = c2
+//a^2 + b^2 = c^2
 //For example, 32 + 42 = 9 + 16 = 25 = 52.
 //
 //There exists exactly one Pythagorean triplet for which a + b + c = 1000.
 //Find the product abc.
 
+
 object Euler9 extends App {
-  def pythagoreanTripletForSum(sum: Int): Seq[(Int, Int, Int)] = {
-    val triplets = for {a <- 1 to sum; b <- 1 to sum; c <- 1 to sum - a - b 
-    	if a * a + b * b == c * c && a + b + c == sum && a < b && b < c} yield (a,b,c)
-    triplets
+  def tripletForSum(sum: Int) = {
+    val triplets = for (a <- (1 to sum).view;
+    b <- 1 to sum - a;
+    c <- 1 to sum - a - b if (isTriplet(a,b,c,sum))) yield (a,b,c)
+    triplets.head
   }
   
-  val triplet = pythagoreanTripletForSum(1000).head
-  println(triplet._1 * triplet._2 * triplet._3)
+  def isTriplet(a: Int, b: Int, c: Int, sum: Int) = {
+    (a*a) + (b*b) == (c*c) && a + b + c == sum && a < b && b < c
+  }
+  
+  val answerTriplet = tripletForSum(1000)
+  println(answerTriplet)
+  val answer = answerTriplet._1 * answerTriplet._2 * answerTriplet._3
+  println(answer)
 }
