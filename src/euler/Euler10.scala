429896dcfb7572877ce54accb2142aea7de34530
shadaj <shadaj>
2012-10-17 18:44:09 -0700

    Added Euler 10 and switched the deprecated Math to math

---
+++ src/euler/Euler10.scala
@@ -0,0 +1,15 @@
+package euler
+
+/* The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
+
+Find the sum of all the primes below two million.
+*/
+
+object Euler10 extends App {
+  def isPrime(num: Long) = {
+    !(2L to math.sqrt(num).toInt).exists(num % _ == 0)
+  }
+  
+  val answer = 2 + (3L to 2000000 by 2).filter(isPrime).sum
+  println(answer)
+}
--- src/euler/Euler7.scala
+++ src/euler/Euler7.scala
@@ -3,9 +3,10 @@ package euler
 //By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
 //
 //What is the 10001st prime number?
+
 object Euler7 extends App {
   def isPrime(num: Int) = {
-    !(2 to Math.sqrt(num).toInt).exists(num % _ == 0)
+    !(2 to math.sqrt(num).toInt).exists(num % _ == 0)
   }
     
   def nextPrimeFrom(n: Int) = {
