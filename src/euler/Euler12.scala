98a441b5e3ae1306940796bfb0c266a34769bb72
shadaj <shadaj>
2012-11-28 18:16:07 -0800

    Added Euler 11 and 12

---
+++ src/euler/Euler12.scala
@@ -0,0 +1,18 @@
+package euler
+
+import Util._
+
+object Euler12 extends App {
+	def factorCount(num: Long): Long = {
+	  primeFactors(num).groupBy(identity).map(_._2.length + 1).product
+	}
+	
+	def triangleNumber(x: Int): Int = {
+	  if (x == 0) 0
+	  else x + triangleNumber(x - 1)
+	}
+	
+	val triangleNumbers = Iterator.from(1).map(triangleNumber)
+	
+	println(triangleNumbers.find(factorCount(_) > 500).get)
+}
